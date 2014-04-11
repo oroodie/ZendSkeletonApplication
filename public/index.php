31ea5804307e6c1a54bae2d59fee93e361122fa6
rgraur <o.roodie@gmail.com>
2014-04-11 15:57:10 +0300

    astartup application: controller, model, view, forms; add/edit/delete/list album items

--- public/index.php
+++ public/index.php
@@ -1,4 +1,12 @@
 <?php
+
+ini_set('xdebug.var_display_max_data', '-1');
+ini_set('xdebug.var_display_max_children', '-1');
+ini_set('xdebug.var_display_max_depth', '-1');
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
